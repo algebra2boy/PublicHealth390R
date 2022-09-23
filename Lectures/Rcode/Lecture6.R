f3625a12028c0c3b9be96a54f477927737560970
Yongye <103079472+algebra2boy@users.noreply.github.com>
2022-09-23 18:31:33 -0400

    HW1 completion

--- Lectures/Rcode/Lecture6.R
+++ Lectures/Rcode/Lecture6.R
@@ -17,3 +17,28 @@ ggplot(data = mpg) +
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x=displ, y =hwy,color = displ <5))
+
+glimpse(mpg)
+
+# displ -> engine displacement, in litres
+# drv -> the type of drive train, where f = front-wheel drive, r = rear wheel drive, 4 = 4wd
+# hwy -> highway miles per gallon
+# class  -> "type" of car
+# year  -> year of manufacture
+
+hist(mpg$year)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color=year ))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color=class ))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size=year ))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size=class ))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy), color = 'blue') 
