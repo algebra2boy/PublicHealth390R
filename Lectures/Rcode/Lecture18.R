77f51aedddeec852419c0c611c7efed6f8fe6a4b
Yongye Tan <yongyetan1209@gmail.com>
2022-12-03 11:44:25 -0500

    Lecture 18

--- Lectures/Rcode/Lecture18.R
+++ Lectures/Rcode/Lecture18.R
@@ -1,3 +1,41 @@
 library(readxl)
-libray
-wages <- read_excel("wages.xlsx",na="NA")
+library(tidyverse)
+wages <- read_excel("/Users/yongyetan/Desktop/College/PublicHealth390R/Lectures/Rcode/wages.xlsx",na="NA")
+
+head(wages)
+
+wages %>% 
+  ggplot(aes(log(income))) +
+  geom_histogram(binwidth = .25)
+
+# the front is the income (y-axis), the back is education (x-axis)
+lm(log(income) ~ education, data = wages)
+
+mod_e <- wages %>% lm(log(income)~ education, data=.) 
+
+library(broom)
+mod_e %>% tidy()
+
+mod_e %>% glance()
+
+mod_e %>% augment()
+
+mod_eh <- wages %>% 
+  lm(log(income) ~ education + height, data = .)
+mod_eh %>% tidy()
+
+mod_eh <- wages %>% 
+  lm(log(income) ~ education + height + sex, data = .)
+mod_eh %>% tidy()
+
+wages %>%
+  ggplot(aes(x = height, y = log(income))) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = lm)
+
+wages %>%
+  ggplot(aes(x = height, y = log(income))) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = loess)
+
+
